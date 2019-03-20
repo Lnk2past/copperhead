4973d9a604cd8fb9f2e7aabee07c7068199ea1cc
Lnk2past <6456398+Lnk2past@users.noreply.github.com>
2019-03-20 11:49:44 +0000

    Add README.md contents to setup.py long_description

--- setup.py
+++ setup.py
@@ -6,12 +6,17 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'copperhead', '__version__.py'), 'r') as f:
     exec(f.read(), about)
 
+with open('README.md', 'r') as f:
+    readme = f.read()
+
 setup(
     name=about['__title__'],
     version=about['__version__'],
     packages=find_packages(),
     author=about['__author__'],
     description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
     license=about['__license__'],
     url=about['__url__']
 )
