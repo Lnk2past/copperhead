034acc291b1b6e6f7918f6faf39630c4326e61de
Lnk2past <Lnk2past@gmail.com>
2019-09-01 18:35:32 -0400

    Fix test script

--- .ci/travis/run_tests.sh
+++ .ci/travis/run_tests.sh
@@ -1,5 +1,6 @@
 set -x
 python examples/basics.py
-python examples/stl_containers.py
+python examples/stl_sequence_containers.py
+python examples/stl_container_adapters.py
 python examples/file.py
 python examples/fractal.py
